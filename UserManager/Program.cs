18351032bf92ade5761735380e2bfc40f70a0874
Jarosław Jusiak <jaroslaw.jusiak@gmail.com>
2019-12-25 22:49:20 +0100

    Log4Net, Cert test.

--- UserManager/Program.cs
+++ UserManager/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -11,9 +13,25 @@ namespace UserManager
 {
     public class Program
     {
+        private static ILoggingBuilder _logging;
+        private static readonly ILogger _logger;
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = CreateHostBuilder(args).Build();
+
+            X509Certificate2 cert = new X509Certificate2(Path.Combine(Directory.GetCurrentDirectory(), "cert.pfx"), "pass123");
+            Console.WriteLine("cert private key: " + cert.PrivateKey);
+
+            try
+            {
+                builder.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
