294b30c2efa537da12638bced4dc38dd564fe85e
Jarosław Jusiak <jaroslaw.jusiak@gmail.com>
2019-12-30 18:46:28 +0100

    User generator based on Bogus library added.

--- UserManager/Data/DbContext.cs
+++ UserManager/Data/DbContext.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Bogus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserManager.Enums;
 using UserManager.Model;
 
 namespace UserManager.Data
@@ -13,10 +15,29 @@ namespace UserManager.Data
         public DbContext()
         {
             _users = new List<User>();
+            SeedUsers();
         }
         public List<User> Users
         {
             get { return _users; }
         }
+
+        #region Seed
+
+        private void SeedUsers()
+        {
+            var userFaker = new Faker<User>()
+                .RuleFor(u => u. Uid, (f, u)=> f.Random.Guid())
+                .RuleFor(u => u.Gender, (f, u) => (Gender)f.Person.Gender)
+                .RuleFor(u => u.FirstName, (f, u) => f.Person.FirstName)
+                .RuleFor(u => u.LastName, (f, u) => f.Person.LastName)
+                .RuleFor(u => u.Email, (f, u) => f.Person.Email)
+                .RuleFor(u => u.Login, (f, u) => f.Person.UserName)
+                .RuleFor(u => u.Password, (f, u) => f.Internet.Password(f.Random.Int(8, 16), false));
+
+            _users.AddRange(userFaker.Generate(5));
+        }
+
+        #endregion
     }
 }
--- UserManager/Model/User.cs
+++ UserManager/Model/User.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserManager.Enums;
 
 namespace UserManager.Model
 {
     public class User
     {
         public Guid Uid { get; set; }
+        public Gender Gender { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
